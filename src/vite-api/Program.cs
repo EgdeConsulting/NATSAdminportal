0b302edbbf79483254f27444be140cf46b9b59b1
TobiasGunther <tobiasg@egde.no>
2023-01-27 12:17:44 +0100

    GH-13: Tried to use Azure App Settings

--- src/vite-api/Program.cs
+++ src/vite-api/Program.cs
@@ -1,13 +1,6 @@
 using System.Text.Json.Nodes;
 using Backend.Logic;
 
-var config = new ConfigurationBuilder()
-    .AddUserSecrets<Program>()
-    .Build();
-
-string? natsServerIp = args.Length != 0 ? args[0] : config["natsServerIp"];
-string? natsServerPort = args.Length != 0 ? args[1] : config["natsServerPort"];
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSpaStaticFiles(config =>
@@ -16,6 +9,21 @@ builder.Services.AddSpaStaticFiles(config =>
 });
 
 var app = builder.Build();
+
+string? natsServerURL;
+
+if (app.Environment.IsDevelopment())
+{
+    var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+    natsServerURL = config["LOCAL_NATS_SERVER_URL"];
+}
+else 
+{
+    natsServerURL = Environment.GetEnvironmentVariable("AZURE_NATS_SERVER_URL");
+}
 
 //var policyName = "enableCORS";
 
@@ -57,7 +65,7 @@ app.UseSpa(builder =>
         builder.UseProxyToSpaDevelopmentServer("http://localhost:5173/");
 });
 
-Subscriber sub = new Subscriber(natsServerIp + ":" + natsServerPort);
+Subscriber sub = new Subscriber(natsServerURL);
 Thread thread = new Thread(sub.Run);
 thread.Start();
 
