cc39f9344bb5c3deb69634c487e723497d8f9db0
Eidsheimen <daniel.eidsheim@gmail.com>
2023-02-22 12:51:31 +0100

    GH-51: Added extra information, fixed policy struct in StreamManager.cs.

--- src/vite-client/src/components/StreamModal.tsx
+++ src/vite-client/src/components/StreamModal.tsx
@@ -7,17 +7,17 @@ import {
   ModalBody,
   ModalFooter,
   Button,
-  Card,
-  CardHeader,
-  CardBody,
   useDisclosure,
+  Heading,
+  Box,
+  Stack,
+  StackDivider,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function StreamModal(props: { content: string }) {
   const { isOpen, onToggle, onClose } = useDisclosure();
-  const [streamName, setStreamName] = useState("");
-  const [data, setData] = useState({});
+  const [streamData, setStreamData] = useState<any>([]);
 
   function sendStreamName(name: string) {
     fetch("/api/streamName", {
@@ -30,19 +30,19 @@ function StreamModal(props: { content: string }) {
     })
       .then((res) => res.json())
       .then((jsonData) => {
-        console.log(jsonData); //This is a string. WHY
+        // jsonData is a string for some reason
+        setStreamData(JSON.parse(jsonData));
       });
   }
 
-  if (isOpen) {
+  useEffect(() => {
     sendStreamName(props.content);
-  }
+  }, [isOpen]);
 
   return (
     <>
       <Button
         onClick={() => {
-          setStreamName(props.content);
           onToggle();
         }}
         variant={"outline"}
@@ -50,29 +50,86 @@ function StreamModal(props: { content: string }) {
       >
         {props.content}
       </Button>
-      <Modal size={"5xl"} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Details for "{props.content}"</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Card>
-              <CardHeader>Subjects</CardHeader>
-              <CardBody></CardBody>
-            </Card>
-            <Card>
-              <CardHeader>Consumers</CardHeader>
-              <CardBody></CardBody>
-            </Card>
-          </ModalBody>
+      {streamData[0] ? (
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>
+              <Heading size={"md"}>Details for "{props.content}"</Heading>
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Stack divider={<StackDivider />} spacing={4}>
+                <Box>
+                  <Heading size={"sm"} marginBottom={2}>
+                    Description
+                  </Heading>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+                  {streamData[0]["Description"].length != 0 ? (
+                    streamData[0]["Description"]
+                  ) : (
+                    <div>No description...</div>
+                  )}
+                </Box>
+                <Box>
+                  <Heading size={"sm"} marginBottom={2}>
+                    Subjects
+                  </Heading>
+
+                  {streamData[0]["Subjects"].map(
+                    (subject: string, key: number) => {
+                      return <div key={key}>{subject}</div>;
+                    }
+                  )}
+                </Box>
+                <Box>
+                  <Heading size={"sm"} marginBottom={2}>
+                    Consumers
+                  </Heading>
+
+                  {streamData[0]["Consumers"].length != 0 ? (
+                    streamData[0]["Consumers"].map(
+                      (consumer: string, key: number) => {
+                        return <div key={key}>{consumer}</div>;
+                      }
+                    )
+                  ) : (
+                    <div>No consumers...</div>
+                  )}
+                </Box>
+                <Box>
+                  <Heading size={"sm"} marginBottom={2}>
+                    Policies
+                  </Heading>
+
+                  {streamData[0]["Policies"].map((policy: any, key: number) => {
+                    return (
+                      <div key={key}>
+                        {/* Probably a better way to display policy string, but this will do for now */}
+                        {JSON.stringify(policy)
+                          .replace(/{|}|"/g, "")
+                          .replace(":", ":\t")}
+                      </div>
+                    );
+                  })}
+                </Box>
+                <Box>
+                  <Heading size={"sm"} marginBottom={2}>
+                    Deleted
+                  </Heading>
+
+                  {<div>Deleted messages: {streamData[0]["Deleted"]}</div>}
+                </Box>
+              </Stack>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      ) : null}
     </>
   );
 }
