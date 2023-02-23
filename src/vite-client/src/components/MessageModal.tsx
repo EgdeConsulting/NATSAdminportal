ae8b617e05aafa263a064f57a80396104fbcc755
Eidsheimen <daniel.eidsheim@gmail.com>
2023-02-23 12:35:09 +0100

    GH-50: Standalone component for subjectdropdown created

--- src/vite-client/src/components/MessageModal.tsx
+++ src/vite-client/src/components/MessageModal.tsx
@@ -32,7 +32,7 @@ function MessageModal() {
           <ModalCloseButton />
           <ModalBody>
             <MessageForm />
-            <Button variant="ghost" mr={3} onClick={onClose}>
+            <Button variant="ghost" mb={2} mr={3} onClick={onClose}>
               Close
             </Button>
           </ModalBody>
