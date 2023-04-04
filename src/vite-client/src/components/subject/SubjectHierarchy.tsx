1609e69b0b783715b46121b72c88a4d7ec7f6d15
Eidsheimen <daniel.eidsheim@gmail.com>
2023-04-04 17:04:55 +0200

    GH-192: Added list and accordion and changed out the checkbox.

--- src/vite-client/src/components/subject/SubjectHierarchy.tsx
+++ src/vite-client/src/components/subject/SubjectHierarchy.tsx
@@ -1,9 +1,23 @@
-import { Checkbox, Card, CardHeader, Heading, VStack } from "@chakra-ui/react";
+import {
+  List,
+  ListItem,
+  Card,
+  CardHeader,
+  Heading,
+  VStack,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
 import { useState, useEffect, memo } from "react";
 import { LoadingSpinner } from "components";
 
 function SubjectHierarchy() {
   const [subjects, setSubjects] = useState<[]>([]);
+  //Wakanda datatype is this?? [] with {name, subsubjects?} ??
+  // interface within here probably
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -16,26 +30,41 @@ function SubjectHierarchy() {
       .then((data) => {
         setSubjects(data);
         setLoading(false);
+        console.log(data);
       });
   }
-
-  const HierarchyCheckbox = memo(
+  // WHATSUP WITH ALL THESE STACK DIVIDERS??????????????????
+  const HierarchyList = memo(
     ({ parent, padding }: { parent: any; padding: number }): JSX.Element => {
       return (
         <>
-          <Checkbox m={1} pl={padding} defaultChecked>
-            {parent.name}
-          </Checkbox>
-          {parent.subSubjects != undefined &&
-            parent.subSubjects.map((child: any) => {
-              return (
-                <HierarchyCheckbox
-                  key={parent.name + child.name}
-                  parent={child}
-                  padding={padding + 5}
-                />
-              );
-            })}
+          <List spacing={3} m={1} pl={padding}>
+            <Accordion allowMultiple>
+              <AccordionItem>
+                {parent.subSubjects != undefined ? (
+                  <AccordionButton>
+                    <ListItem>{parent.name}</ListItem>
+                    <AccordionIcon />
+                  </AccordionButton>
+                ) : (
+                  <ListItem>{parent.name}</ListItem>
+                )}
+                <AccordionPanel pb={4}>
+                  {parent.subSubjects != undefined &&
+                    parent.subSubjects.map((child: any) => {
+                      console.log(child.subSubjects);
+                      return (
+                        <HierarchyList
+                          key={parent.name + child.name}
+                          parent={child}
+                          padding={padding + 5}
+                        />
+                      );
+                    })}
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </List>
         </>
       );
     }
@@ -59,7 +88,7 @@ function SubjectHierarchy() {
           {subjects.map((subject: any, index: number) => {
             return (
               <VStack key={index} ml={4} mb={3} alignItems={"left"}>
-                <HierarchyCheckbox parent={subject} padding={0} />
+                <HierarchyList parent={subject} padding={0} />
               </VStack>
             );
           })}
